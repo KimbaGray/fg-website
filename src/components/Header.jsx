9aecf0bc1760899a9e2fe53276df5a447270ccf1
Kim Gray <kim.lc.gray@gmail.com>
2020-10-10 14:18:10 +0100

    added padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Logo from "../images/Logo/Logo.png";
+import Logo1 from "../images/Logo/Logo.svg";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
@@ -8,7 +9,7 @@ class Navigation extends Component {
     return (
       <div className="customNav">
         <Link className="navbar-brand" to="/">
-          <img src={Logo} alt="Felicity Gray Stylist" className="logo" />
+          <img src={Logo1} alt="Felicity Gray Stylist" className="logo" />
         </Link>
       </div>
     );
